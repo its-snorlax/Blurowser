409a285b2ae9d4daed0badbc10c805d77021e9f9
Prateek Jain <prateek.jain23995@yahoo.com>
2019-10-12 11:53:11 +0530

    Introducing Ctrl+B and Ctrl+C to activate and inactivate blurowser

--- js/background.js
+++ js/background.js
@@ -1,3 +1,11 @@
+chrome.commands.onCommand.addListener(function (command) {
+    chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, {action: command}, function (response) {
+        });
+    });
+});
+
+
 chrome.runtime.onInstalled.addListener(function () {
     chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
         chrome.declarativeContent.onPageChanged.addRules([{

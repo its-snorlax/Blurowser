5ee742050ffadd9fd8ea13d17b994ee119aeb9a9
Prayas Jain <p.jain3322@gmail.com>
2019-10-12 20:46:20 +0530

    allow user to select element by hovering after pressing ctrl + b.

--- js/script.js
+++ js/script.js
@@ -1,18 +1,28 @@
+let previousElement = null;
+let onmouseover = window.onmouseover;
+
 function activeBlurowser() {
-  const body = document.getElementsByTagName("body")[0];
-  body.classList.add("blur-active")
+    window.onmouseover = function (e) {
+        const currentElement = e.target;
+        if (previousElement === null) {
+            currentElement.classList.add("element-selector");
+            previousElement = currentElement;
+        } else {
+          currentElement.classList.add("element-selector");
+            previousElement.classList.remove("element-selector");
+            previousElement = currentElement
+        }
+    }
 }
 
 function inactiveBlurowser() {
-  const body = document.getElementsByTagName("body")[0];
-  body.classList.remove("blur-active")
+  window.onmouseover = onmouseover
 }
 
-
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  if(message.action === "activate-blurowser") {
-    activeBlurowser();
-    return;
-  }
+    if (message.action === "activate-blurowser") {
+      activeBlurowser();
+        return;
+    }
   inactiveBlurowser();
 });
